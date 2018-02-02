f468d30e8157e445c9c4204cb4cd3676ccc65d14
pwatson1 <pawdirect@gmail.com>
2018-02-02 07:38:21 -0500

    cservlog: Add logs to credit card service

--- src/main/java/com/zipcode/transcurrency/Transcurrency/services/CreditCardService.java
+++ src/main/java/com/zipcode/transcurrency/Transcurrency/services/CreditCardService.java
@@ -2,11 +2,12 @@ package com.zipcode.transcurrency.Transcurrency.services;
 
 import com.zipcode.transcurrency.Transcurrency.models.CreditCard;
 import com.zipcode.transcurrency.Transcurrency.repositories.CreditCardRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +15,8 @@ import java.util.List;
 
 @Service
 public class CreditCardService {
+    private static final Logger logger = LoggerFactory.getLogger(CreditCardService.class);
+
 
     private CreditCardRepository creditCardRepository;
 
@@ -23,6 +26,7 @@ public class CreditCardService {
 
 
     public List<CreditCard> getAllCreditCards() {
+        logger.info("Retrieving all creditCards.");
         List<CreditCard> creditCards = new ArrayList<>();
         creditCardRepository.findAll()
                 .forEach(creditCards::add);
@@ -31,7 +35,7 @@ public class CreditCardService {
 
     //creates a credit card
     public HttpHeaders createCreditCard(CreditCard creditCard) {
-
+        logger.info("New creditCard generated.");
         creditCard = creditCardRepository.save(creditCard);
 
         HttpHeaders responseHeaders = new HttpHeaders();
@@ -47,16 +51,19 @@ public class CreditCardService {
 
     //gets a single credit card
     public CreditCard getCreditCard(Long creditCardId) {
+        logger.info("CreditCard retrieved.");
         return creditCardRepository.findOne(creditCardId);
     }
 
     //updates credit card info
     public CreditCard updateCreditCard(CreditCard creditCard, Long creditCardId) {
+        logger.info("CreditCard info modified.");
         return creditCardRepository.save(creditCard);
     }
 
     //deletes a credit card
     public Boolean deleteCreditCard(Long creditCardId) {
+        logger.info("CreditCard deleted.");
         creditCardRepository.delete(creditCardId);
         return true;
     }
